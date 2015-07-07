55007eba6eaf3cb37f8aa95d9b4c6561cffb3d41
Diogo Souto <souto@on.br>
2015-07-07 22:42:37 +0100

    auto.py

--- auto.py
+++ auto.py
@@ -1,22 +1,61 @@
+#!
+
+'''auto.py: main script to obtain automatic abundances for cool dwarfs
+   	    using the turbospectrum code from B. Plez.
+   	    
+   	    This code consist in obtain the stellar abundance using the chi-square
+   	    method to find a best fit between the synthetic and observed spectra.
+
+	'''
+
+__author__ = 'Diogo Souto'
+__email__  = 'souto.at.on.br'
+
+ 
+###################################
+#       Importing modules         #
+###################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pyconvert
 import glob as glob
 
+###################################
+#       Auxiliary Methods         #
+###################################
+
+
+###################################
+#          Run script 	          #
+###################################
+
+
 def run():	
 
+	# Set the path of the system 
+	os.chdir('/home/diogo/turbopipe/turbospec_script/')					
 
-	os.chdir('/home/diogo/turbopipe/turbospec_script/')					#set the path of system
-	os.system('csh -f auto_dwarfs_O.com')							#run turbospectrum script
+	# Run turbospectrum script - The first element should be Oxygen, because there is several lines of.
+	os.system('csh -f auto_dwarfs_O.com')							
 	
-	os.chdir('/home/diogo/turbopipe/trash/')						#set the path of system
+	# Set the path of the system
+	os.chdir('/home/diogo/turbopipe/trash/')
+	
+	# Searching for synthetic spectra 		
 	Oxygen_syn = glob.glob('teste_trash_Oxygen_*.txt')
-	for a in Oxygen_syn:
-		pyconvert.faltbo(a,"convol"+a,710.,2);print 'Making a gaussian profile to ', a	#runing a gaussian smooth
-		
-			
 	
+	# The pyconvert.faltbo it's a python version of the faltbo.f90 file from turbospectrum, B. Plez
+	for a in Oxygen_syn:
+		pyconvert.faltbo(a,"convol_"+a,710.,2);print 'Making a gaussian profile to ', a	
+		
+
+	# Open main folder and run chi2 method
+	os.chdir('/home/diogo/turbopipe/')	
+	os.system('python chi2.py')
+
+
 	print 'Program finished well'
 	return
 	
